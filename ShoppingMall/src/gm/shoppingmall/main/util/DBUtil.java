1579f8ccb3e4ada50c7349c9a5d4daf47c196900
WooSungHwan <doqndnffo@gmail.com>
2018-11-08 18:32:06 +0900

    DB

---
+++ ShoppingMall/src/gm/shoppingmall/main/util/DBUtil.java
@@ -0,0 +1,26 @@
+package gm.shoppingmall.main.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * 디비 연결을 위한 클래스
+ * @author 우성환
+ *
+ */
+public class DBUtil {
+	
+	public static Connection getConnection() {
+		Connection conn = null;
+		
+		String url = "jdbc:oracle:thin:@211.63.89.64:1521:xe";
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(url,"gmshop","java1234");
+		} catch (Exception e) {
+			System.out.println("DBUtil.getConnection :" + e.toString());
+		}
+		return conn;
+	}
+}
